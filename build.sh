86fa28cd1dec91cde1cb73c36e43526516149ee6
Gagan Kaushik <gagan.kaushik1@gmail.com>
2024-04-02 03:59:53 -0700

    updated build and run scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+VERSION=0.1
+IMG_NAME="us-west1-docker.pkg.dev/buzzer-418603/buzzerapp/buzzerimg:$VERSION"
+
 if [ "$1" == "prod" ]; then
-    docker build -t gaganbet .
+    docker build --platform=linux/amd64 -t $IMG_NAME .
+    docker push $IMG_NAME
 else
-    docker build -t gaganbet .
+    docker build -t $IMG_NAME .
 fi
--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+VERSION=0.1
+IMG_NAME="us-west1-docker.pkg.dev/buzzer-418603/buzzerapp/buzzerimg:$VERSION"
+
 docker run \
     -v $(readlink -f buzzer-418603-732db8e02147.json):/workspace/buzzer-418603-732db8e02147.json \
     -e GOOGLE_APPLICATION_CREDENTIALS="buzzer-418603-732db8e02147.json" \
     --rm \
     -it \
     -p 8000:8000 \
-    docker.io/library/gaganbet
+    $IMG_NAME
